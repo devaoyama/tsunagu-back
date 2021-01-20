bd8f888425e63c2d4fa4c194f4de384b9997447b
devkeita <keita.mywork@gmail.com>
2021-01-20 21:41:16 +0900

    make ParticipantController

--- app/Participant.php
+++ app/Participant.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'group_id',
+    ];
+
     public function user()
     {
         $this->belongsTo(User::class);
