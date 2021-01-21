0179a3454fc639f287243df80dcd43431735f3e0
devkeita <keita.mywork@gmail.com>
2021-01-21 21:53:08 +0900

    make ParticipantStatusType enum and add field

--- database/migrations/2021_01_19_121127_create_participants_table.php
+++ database/migrations/2021_01_19_121127_create_participants_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\ParticipantStatusType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ class CreateParticipantsTable extends Migration
             $table->bigIncrements('id');
             $table->integer('user_id')->unsigned();
             $table->integer('group_id')->unsigned();
+            $table->string('status')->default(ParticipantStatusType::Waiting);
             $table->timestamps();
         });
     }
